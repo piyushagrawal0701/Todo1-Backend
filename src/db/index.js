fca2f4ba5d5d40baea9c9b21785c78988d970e40
AyushAgrawal9131 <ayushagrawal9131@gmail.com>
2024-07-31 10:13:18 +0530

    mongodb file changed

--- src/db/index.js
+++ src/db/index.js
@@ -1,19 +1,37 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
-const DB_NAME = require("../constants.js");
+const mongoose = require('mongoose');
 
-const connectDB = () => {
+const connectDB = async () => {
   try {
-    mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    console.log("Mongo DB connected");
-
-    // app.listen(process.env.PORT, () => {
-    //   console.log(`Port is running at ${process.env.PORT}`);
-    // });
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
   } catch (error) {
-    console.error("MongoDB Connection ERROR: ", error);
+    console.error('MongoDB connection error:', error);
     throw error;
   }
 };
 
 module.exports = connectDB;
+
+
+// require("dotenv").config();
+// const mongoose = require("mongoose");
+// const DB_NAME = require("../constants.js");
+
+// const connectDB = () => {
+//   try {
+//     mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     console.log("Mongo DB connected");
+
+//     // app.listen(process.env.PORT, () => {
+//     //   console.log(`Port is running at ${process.env.PORT}`);
+//     // });
+//   } catch (error) {
+//     console.error("MongoDB Connection ERROR: ", error);
+//     throw error;
+//   }
+// };
+
+// module.exports = connectDB;
